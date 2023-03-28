88414e27cfbf13f5cc043d4cf75e7941edae9c6f
Nikhil Chadha <nikhilchadha1537@gmail.com>
2023-03-28 19:09:58 +0530

    Connected Mysql

---
+++ sport_management/sport_management/models.py
@@ -0,0 +1,4 @@
+from django.db import connections
+from django.db import models
+
+class Students()
--- sport_management/sport_management/settings.py
+++ sport_management/sport_management/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = "sport_management.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": 'sports',
+        'USER': 'root',
+        'PASSWORD': 'nikhil2002',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
